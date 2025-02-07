8f0e496c18eb2a752dca031ae3e6e1221924b64e
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-02-07 16:50:36 +0530

    feat: added routes using react-router-dom

---
+++ frontend/src/routes/AppRoute.jsx
@@ -0,0 +1,14 @@
+import { Route, BrowserRouter,  Routes } from "react-router-dom"
+const AppRoute = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<div>Home</div>}/>
+        <Route path="/login" element={<div>Login</div>}/>
+        <Route path="/register" element={<div >Register</div>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoute
